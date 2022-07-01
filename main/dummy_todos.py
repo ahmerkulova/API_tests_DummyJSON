0124b121d2300cd4631049a9f37b49b604419351
Arina <ahmerkulova@gmail.com>
2022-07-01 16:30:23 +0300

    add test to validate json schema

--- main/dummy_todos.py
+++ main/dummy_todos.py
@@ -1,12 +1,25 @@
-import pytest
 import requests
+import json
+from cerberus import Validator
 
-BASE_URL = 'https://dummyjson.com/todos'
+from config import BASE_URL, todo_id
 
 
 class DummyTodos:
     def __init__(self):
         self.base_url = BASE_URL
+        self.id = todo_id
 
     def get_all_todos(self):
         return requests.get(self.base_url)
+
+    def get_todo_by_id(self):
+        return requests.get(self.base_url + self.id)
+
+    @staticmethod
+    def validate_json_schema(response, schema):
+        todo = json.loads(response.text)
+
+        v = Validator(schema)
+        is_valid = v.validate(todo)
+        return is_valid, v.errors
--- tests/test_get_todos.py
+++ tests/test_get_todos.py
@@ -1,9 +1,22 @@
+from assertpy import assert_that
+
 from main.dummy_todos import DummyTodos
+from tests.data.schemas import *
 
 todos = DummyTodos()
 
 
 def test_get_all_todos():
     response = todos.get_all_todos()
-    assert response.status_code == 200, f'Status code is {response.status_code}, should be 200'
+    assert_that(response.status_code).is_equal_to(200)
 
+
+def test_get_todo_by_id():
+    response = todos.get_todo_by_id()
+    assert_that(response.status_code).is_equal_to(200)
+
+
+def test_todo_by_id_has_expected_schema():
+    response = todos.get_todo_by_id()
+    is_valid, errors = todos.validate_json_schema(response, todo_id_schema)
+    assert_that(is_valid).described_as(errors).is_true()
