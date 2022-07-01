7014283d1036b50a2cde2b29a0791b26e7f16f1a
Arina <ahmerkulova@gmail.com>
2022-07-01 23:49:38 +0300

    add helpers file with validation func

---
+++ main/help_functions.py
@@ -0,0 +1,9 @@
+import json
+from cerberus import Validator
+
+
+def validate_json_schema(response, schema):
+    todo = json.loads(response.text)
+    v = Validator(schema)
+    is_valid = v.validate(todo)
+    return is_valid, v.errors
