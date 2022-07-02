d9ca00ff47eac1ad6aaa33844e395f36cd3038ef
Arina <ahmerkulova@gmail.com>
2022-07-02 15:39:54 +0300

    add schema for DELETE method response

--- tests/data/schemas.py
+++ tests/data/schemas.py
@@ -1,7 +1,18 @@
 schema_todo_id = {
-   "id": {'type': 'integer'},
-   "todo": {'type': 'string'},
-   "completed": {'type': 'boolean'},
-   "userId": {'type': 'integer'}
+    "id": {'type': 'integer'},
+    "todo": {'type': 'string'},
+    "completed": {'type': 'boolean'},
+    "userId": {'type': 'integer'}
 }
 
+deletion_datetime_regex = '^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9])' \
+                          r':([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$'
+
+schema_deleted_todo = {
+    "id": {'type': 'integer'},
+    "todo": {'type': 'string'},
+    "completed": {'type': 'boolean'},
+    "userId": {'type': 'integer'},
+    "isDeleted": {'type': 'boolean'},
+    "deletedOn": {'type': 'string', 'regex': deletion_datetime_regex}
+}
