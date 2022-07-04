6b167d0bfefbff45b933b332cbb000aba055a243
Arina <ahmerkulova@gmail.com>
2022-07-04 11:49:58 +0300

    add negative checks to GET method

--- tests/test_get_todos.py
+++ tests/test_get_todos.py
@@ -32,3 +32,21 @@ def test_todo_by_id_has_expected_schema(id=TODO_ID):
     response = todos.get_todo_by_id(id)
     is_valid, errors = validate_json_schema(response, schema_todo_id)
     assert_that(is_valid).described_as(errors).is_true()
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize('id', ('0', str(TODOS_TOTAL + 1)))
+def test_guest_cant_get_todo_by_id_out_of_range(id):
+    response = todos.get_todo_by_id(id)
+    body = serialized(response)
+    assert_that(response.status_code).is_equal_to(404)
+    assert_that(body).contains_value(f"Todo with id '{id}' not found")
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize('id', ('3.14', 'True'))
+def test_guest_cant_get_todo_by_invalid_id_type(id):
+    response = todos.get_todo_by_id(id)
+    body = serialized(response)
+    assert_that(response.status_code).is_equal_to(404)
+    assert_that(body).contains_value(f"Todo with id '{id}' not found")
