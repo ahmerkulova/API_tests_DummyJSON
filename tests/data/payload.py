2a8ffbcb78b8476ed792734823f39fb73278b34d
Arina <ahmerkulova@gmail.com>
2022-07-04 13:20:13 +0300

    add negative checks for PUT method

--- tests/data/payload.py
+++ tests/data/payload.py
@@ -1,11 +1,19 @@
 UPDATE_TODO = "Lay down your arms, give up the fight"
 UPDATE_STATUS = False
 UPDATE_USERID = 21
+UPDATE_NEW_PARAMETER = "I don't belong here"
 
 update_payload = {
     "todo": UPDATE_TODO,
     "completed": UPDATE_STATUS,
     "userId": UPDATE_USERID
+}
+
+update_payload_new_parameter = {
+    "todo": UPDATE_TODO,
+    "completed": UPDATE_STATUS,
+    "userId": UPDATE_USERID,
+    "new_parameter": UPDATE_NEW_PARAMETER
 }
 
 CREATE_TODO = "Imagine there's no heaven"
--- tests/test_update_todos.py
+++ tests/test_update_todos.py
@@ -3,22 +3,40 @@ from assertpy import assert_that
 from main.dummy_todos import DummyTodos
 from main.help_functions import *
 from tests.data.payload import *
-from config import TODO_ID
+from config import *
 
 todos = DummyTodos()
 
 
 @pytest.mark.smoke
-def test_guest_can_update_todo_by_id(id=TODO_ID):
-    response = todos.update_todo_by_id(id, deserialized(update_payload))
+def test_guest_can_update_todo_by_id(id=TODO_ID, payload=update_payload):
+    response = todos.update_todo_by_id(id, deserialized(payload))
     assert_that(response.text).is_not_empty()
     assert_that(response.status_code).is_equal_to(200)
 
 
 @pytest.mark.smoke
-def test_all_updated_fields_are_correct(id=TODO_ID):
-    response = todos.update_todo_by_id(id, deserialized(update_payload))
+def test_all_updated_fields_are_correct(id=TODO_ID, payload=update_payload):
+    response = todos.update_todo_by_id(id, deserialized(payload))
     body = serialized(response)
     assert_that(body).has_todo(UPDATE_TODO)
     assert_that(body).has_completed(UPDATE_STATUS)
     assert_that(body).has_userId(UPDATE_USERID)
+
+
+@pytest.mark.negative
+def test_guest_cant_update_todo_with_unexpected_parameter(id=TODO_ID, payload=update_payload_new_parameter):
+    response = todos.update_todo_by_id(id, deserialized(payload))
+    body = serialized(response)
+    print(body)
+    assert_that(body).does_not_contain_value(UPDATE_NEW_PARAMETER)
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize('id', ('0', str(TODOS_TOTAL + 1)))
+def test_guest_cant_update_todo_out_of_range(id, payload=update_payload):
+    response = todos.update_todo_by_id(id, deserialized(payload))
+    body = serialized(response)
+    assert_that(response.status_code).is_equal_to(404)
+    assert_that(body).contains_value(f"Todo with id '{id}' not found")
+    
